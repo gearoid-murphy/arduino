80dfaa453562a58f193e5294c9e806e7b0e5b941
Gearoid Murphy <gearoid.p.murphy@gmail.com>
2023-06-27 13:56:29 -0700

    Successful color sensor test run.

--- TCS34725/TCS34725.ino
+++ TCS34725/TCS34725.ino
@@ -1,73 +1,34 @@
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
 
-// Pick analog outputs, for the UNO these three work well
-// use ~560  ohm resistor between Red & Blue, ~1K for green (its brighter)
-#define redpin 3
-#define greenpin 5
-#define bluepin 6
-// for a common anode LED, connect the common pin to +5V
-// for common cathode, connect the common to ground
-
-// set to false if using a common cathode LED
-#define commonAnode true
-
-// our RGB -> eye-recognized gamma color
-byte gammatable[256];
-
-
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_4X);
 
 void setup() {
-  Serial.begin(9600);
-  //Serial.println("Color View Test!");
+  Serial.begin(115200);
+  Serial.println("Color View Test!");
 
   if (tcs.begin()) {
-    //Serial.println("Found sensor");
+    Serial.println("Found sensor");
   } else {
     Serial.println("No TCS34725 found ... check your connections");
     while (1); // halt!
-  }
-
-  // use these three pins to drive an LED
-
-  pinMode(redpin, OUTPUT);
-  pinMode(greenpin, OUTPUT);
-  pinMode(bluepin, OUTPUT);
-
-  // thanks PhilB for this gamma table!
-  // it helps convert RGB colors to what humans see
-  for (int i = 0; i < 256; i++) {
-    float x = i;
-    x /= 255;
-    x = pow(x, 2.5);
-    x *= 255;
-
-    if (commonAnode) {
-      gammatable[i] = 255 - x;
-    } else {
-      gammatable[i] = x;
-    }
-    //Serial.println(gammatable[i]);
   }
 }
 
 
 void loop() {
-  float red, green, blue;
-  tcs.setInterrupt(false);  // turn on LED
-  delay(60);  // takes 50ms to read
-  tcs.getRGB(&red, &green, &blue);
-  tcs.setInterrupt(true);  // turn off LED
+  uint16_t r, g, b, c, colorTemp, lux;
 
-  Serial.print("R:\t"); Serial.print(int(red));
-  Serial.print("\tG:\t"); Serial.print(int(green));
-  Serial.print("\tB:\t"); Serial.print(int(blue));
+  tcs.getRawData(&r, &g, &b, &c);
+  // colorTemp = tcs.calculateColorTemperature(r, g, b);
+  // colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
+  // lux = tcs.calculateLux(r, g, b);
 
-  Serial.print("\n");
-
-  analogWrite(redpin, gammatable[(int)red]);
-  analogWrite(greenpin, gammatable[(int)green]);
-  analogWrite(bluepin, gammatable[(int)blue]);
-
+  // Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
+  // Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
+  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
+  Serial.println(" ");
 }
